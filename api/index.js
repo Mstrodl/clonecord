dff3f1d5a1180ce78000195529e27daea55b83da
FNCxPro <relative@outlook.ie>
2017-07-13 19:16:02 -0400

    rest api start

--- api/index.js
+++ api/index.js
@@ -1,2 +1,12 @@
-const express = require('express')
-console.log('the API is not complete.')
+const express = require('express'),
+      config = require('../config/')
+
+const app = require('express')
+
+
+app.use((err, req, res, next) => {
+  console.error(err)
+  res.send('Oops error')
+})
+app.disable('x-powered-by')
+app.listen(config.services.api.port, console.log(`Listening on ${config.services.api.route || config.services.api.route}:${config.services.api.port}`))
